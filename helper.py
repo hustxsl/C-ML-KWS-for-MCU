32c4a571aa3d4492de77d1cf2efde1b90e1dbc08
Jeffy Chen <jeffy.chen@rock-chips.com>
2019-08-05 19:15:40 +0800

    Add quant_dump script to dump quant params
    Base on https://github.com/tpeet/ML-KWS-for-MCU
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ helper.py
@@ -0,0 +1,193 @@
+# Copyright 2017 Tanel Peet. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import math
+import cmath
+import numpy as np
+import os
+
+def write_ds_cnn_h_beginning(f, wanted_words, sample_rate, clip_duration_ms,
+                             window_size_ms, window_stride_ms, dct_coefficient_count,
+                             model_size_info, act_max):
+  f.write("#ifndef __DS_CNN_H__\n")
+  f.write("#define __DS_CNN_H__\n\n")
+  f.write('#include "nn.h"\n')
+  f.write('#include "ds_cnn_weights.h"\n')
+  f.write('#include "local_NN.h"\n')
+  f.write('#include "arm_math.h"\n\n')
+  desired_samples = int(sample_rate * clip_duration_ms / 1000)
+  window_size_samples = int(sample_rate * window_size_ms / 1000)
+  window_stride_samples = int(sample_rate * window_stride_ms / 1000)
+  length_minus_window = (desired_samples - window_size_samples)
+  if length_minus_window < 0:
+    spectrogram_length = 0
+  else:
+    spectrogram_length = 1 + int(length_minus_window / window_stride_samples)
+
+  input_x = dct_coefficient_count
+  input_y = spectrogram_length
+
+  f.write("#define SAMP_FREQ {}\n".format(sample_rate))
+  f.write("#define MFCC_DEC_BITS {}\n".format(int(7 - np.log2(act_max[0]))))
+  f.write("#define FRAME_SHIFT_MS {}\n".format(int(window_stride_ms)))
+  f.write("#define FRAME_SHIFT ((int16_t)(SAMP_FREQ * 0.001 * FRAME_SHIFT_MS))\n")
+  f.write("#define NUM_FRAMES {}\n".format(spectrogram_length))
+  f.write("#define NUM_MFCC_COEFFS {}\n".format(dct_coefficient_count))
+  f.write("#define FRAME_LEN_MS {}\n".format(int(window_size_ms)))
+  f.write("#define FRAME_LEN ((int16_t)(SAMP_FREQ * 0.001 * FRAME_LEN_MS))\n\n")
+
+  f.write("#define IN_DIM (NUM_FRAMES*NUM_MFCC_COEFFS)\n")
+  f.write("#define OUT_DIM {}\n\n".format(int(len(wanted_words.split(',')) + 2)))
+
+  num_layers = model_size_info[0]
+  i = 1
+  for layer_no in range(1, num_layers + 1):
+    f.write("#define CONV{}_OUT_CH {}\n".format(layer_no, model_size_info[i]))
+    i += 1
+    ky = model_size_info[i]
+    i += 1
+    kx = model_size_info[i]
+    i += 1
+    sy = model_size_info[i]
+    i += 1
+    sx = model_size_info[i]
+    out_x = math.ceil(float(input_x) / float(sx))
+    out_y = math.ceil(float(input_y) / float(sy))
+    pad_x = max((out_x - 1) * sx + kx - input_x, 0) // 2
+    pad_y = max((out_y - 1) * sy + ky - input_y, 0) // 2
+    if layer_no == 1:
+      f.write("#define CONV1_IN_X NUM_MFCC_COEFFS\n")
+      f.write("#define CONV1_IN_Y NUM_FRAMES\n")
+      f.write("#define CONV{}_KX {}\n".format(layer_no, kx))
+      f.write("#define CONV{}_KY {}\n".format(layer_no, ky))
+      f.write("#define CONV{}_SX {}\n".format(layer_no, sx))
+      f.write("#define CONV{}_SY {}\n".format(layer_no, sy))
+      f.write("#define CONV{}_PX {}\n".format(layer_no, pad_x))
+      f.write("#define CONV{}_PY {}\n".format(layer_no, pad_y))
+      f.write("#define CONV{}_OUT_X {}\n".format(layer_no, int(out_x)))
+      f.write("#define CONV{}_OUT_Y {}\n".format(layer_no, int(out_y)))
+
+    else:
+      f.write("#define CONV{1}_IN_X CONV{0}_OUT_X\n".format(layer_no - 1, layer_no))
+      f.write("#define CONV{1}_IN_Y CONV{0}_OUT_Y\n".format(layer_no - 1, layer_no))
+      f.write("#define CONV{}_DS_KX {}\n".format(layer_no, kx))
+      f.write("#define CONV{}_DS_KY {}\n".format(layer_no, ky))
+      f.write("#define CONV{}_DS_SX {}\n".format(layer_no, sx))
+      f.write("#define CONV{}_DS_SY {}\n".format(layer_no, sy))
+      f.write("#define CONV{}_DS_PX {}\n".format(layer_no, int(pad_x)))
+      f.write("#define CONV{}_DS_PY {}\n".format(layer_no, int(pad_y)))
+      f.write("#define CONV{0}_OUT_X {1}\n".format(layer_no, int(out_x)))
+      f.write("#define CONV{0}_OUT_Y {1}\n".format(layer_no, int(out_y)))
+
+    i += 1
+    f.write("\n")
+    input_x = out_x
+    input_y = out_y
+
+def write_ds_cnn_h_end(f, num_layers):
+  f.write(
+    '#define SCRATCH_BUFFER_SIZE (2*2*CONV1_OUT_CH*CONV2_DS_KX*CONV2_DS_KY + 2*CONV2_OUT_CH*CONV2_OUT_X*CONV2_OUT_Y)\n\n')
+  f.write('class DS_CNN : public NN {\n\n')
+  f.write('  public:\n')
+  f.write('    DS_CNN();\n')
+  f.write('    ~DS_CNN();\n')
+  f.write('    void run_nn(q7_t* in_data, q7_t* out_data);\n\n')
+  f.write('  private:\n')
+  f.write('    q7_t* scratch_pad;\n')
+  f.write('    q7_t* col_buffer;\n')
+  f.write('    q7_t* buffer1;\n')
+  f.write('    q7_t* buffer2;\n')
+  f.write('    static q7_t const conv1_wt[CONV1_OUT_CH*CONV1_KX*CONV1_KY];\n')
+  f.write('    static q7_t const conv1_bias[CONV1_OUT_CH];\n')
+
+  for layer_no in range(2, num_layers + 1):
+    f.write(
+      '    static q7_t const conv{1}_ds_wt[CONV{0}_OUT_CH*CONV{1}_DS_KX*CONV{1}_DS_KY];\n'.format(layer_no - 1, layer_no))
+    f.write('    static q7_t const conv{1}_ds_bias[CONV{0}_OUT_CH];\n'.format(layer_no - 1, layer_no))
+    f.write('    static q7_t const conv{1}_pw_wt[CONV{1}_OUT_CH*CONV{0}_OUT_CH];\n'.format(layer_no - 1, layer_no))
+    f.write('    static q7_t const conv{0}_pw_bias[CONV{0}_OUT_CH];\n'.format(layer_no))
+
+  f.write('    static q7_t const final_fc_wt[CONV{}_OUT_CH*OUT_DIM];\n'.format(num_layers))
+  f.write('    static q7_t const final_fc_bias[OUT_DIM];\n\n')
+  f.write('};\n\n')
+  f.write('#endif\n')
+
+def write_ds_cnn_cpp_file(fname, num_layers):
+  f = open(fname, 'wb')
+  f.close()
+  with open(fname, 'a') as f:
+    f.write('#include "ds_cnn.h"\n\n')
+
+    for layer_no in range(0, num_layers):
+      if layer_no == 0:
+        f.write("const q7_t DS_CNN::conv1_wt[CONV1_OUT_CH*CONV1_KX*CONV1_KY]=CONV1_WT;\n")
+        f.write("const q7_t DS_CNN::conv1_bias[CONV1_OUT_CH]=CONV1_BIAS;\n")
+      else:
+        f.write(
+          "const q7_t DS_CNN::conv{1}_ds_wt[CONV{0}_OUT_CH*CONV{1}_DS_KX*CONV{1}_DS_KY]=CONV{1}_DS_WT;\n".format(layer_no, layer_no + 1))
+        f.write("const q7_t DS_CNN::conv{1}_ds_bias[CONV{0}_OUT_CH]=CONV{1}_DS_BIAS;\n".format(layer_no, layer_no + 1))
+        f.write(
+          "const q7_t DS_CNN::conv{1}_pw_wt[CONV{1}_OUT_CH*CONV{0}_OUT_CH]=CONV{1}_PW_WT;\n".format(layer_no, layer_no + 1))
+        f.write("const q7_t DS_CNN::conv{0}_pw_bias[CONV{0}_OUT_CH]=CONV{0}_PW_BIAS;\n".format(layer_no + 1))
+
+    f.write("const q7_t DS_CNN::final_fc_wt[CONV{0}_OUT_CH*OUT_DIM]=FINAL_FC_WT;\n".format(num_layers))
+    f.write("const q7_t DS_CNN::final_fc_bias[OUT_DIM]=FINAL_FC_BIAS;\n\n")
+
+    f.write("DS_CNN::DS_CNN()\n")
+    f.write("{\n")
+    f.write("  scratch_pad = new q7_t[SCRATCH_BUFFER_SIZE];\n")
+    f.write("  buffer1 = scratch_pad;\n")
+    f.write("  buffer2 = buffer1 + (CONV1_OUT_CH*CONV1_OUT_X*CONV1_OUT_Y);\n")
+    f.write("  col_buffer = buffer2 + (CONV2_OUT_CH*CONV2_OUT_X*CONV2_OUT_Y);\n")
+    f.write("  frame_len = FRAME_LEN;\n")
+    f.write("  frame_shift = FRAME_SHIFT;\n")
+    f.write("  num_mfcc_features = NUM_MFCC_COEFFS;\n")
+    f.write("  num_frames = NUM_FRAMES;\n")
+    f.write("  num_out_classes = OUT_DIM;\n")
+    f.write("  in_dec_bits = MFCC_DEC_BITS;\n")
+    f.write("}\n\n")
+
+    f.write("DS_CNN::~DS_CNN()\n")
+    f.write("{\n")
+    f.write("  delete scratch_pad;\n")
+    f.write("}\n\n")
+
+    f.write("void DS_CNN::run_nn(q7_t* in_data, q7_t* out_data)\n")
+    f.write("{\n")
+    for layer_no in range(0, num_layers):
+      if layer_no == 0:
+        f.write("  //CONV1 : regular convolution\n")
+        f.write(
+          "  arm_convolve_HWC_q7_basic_nonsquare(in_data, CONV1_IN_X, CONV1_IN_Y, 1, conv1_wt, CONV1_OUT_CH, CONV1_KX, CONV1_KY, CONV1_PX, CONV1_PY, CONV1_SX, CONV1_SY, conv1_bias, CONV1_BIAS_LSHIFT, CONV1_OUT_RSHIFT, buffer1, CONV1_OUT_X, CONV1_OUT_Y, (q15_t*)col_buffer, NULL);\n")
+        f.write("  arm_relu_q7(buffer1,CONV1_OUT_X*CONV1_OUT_Y*CONV1_OUT_CH);\n\n")
+      else:
+        f.write("  //CONV{} : DS + PW conv\n".format(layer_no + 1))
+        f.write("  //Depthwise separable conv (batch norm params folded into conv wts/bias)\n")
+        f.write(
+          "  arm_depthwise_separable_conv_HWC_q7_nonsquare(buffer1,CONV{1}_IN_X,CONV{1}_IN_Y,CONV{0}_OUT_CH,conv{1}_ds_wt,CONV{0}_OUT_CH,CONV{1}_DS_KX,CONV{1}_DS_KY,CONV{1}_DS_PX,CONV{1}_DS_PY,CONV{1}_DS_SX,CONV{1}_DS_SY,conv{1}_ds_bias,CONV{1}_DS_BIAS_LSHIFT,CONV{1}_DS_OUT_RSHIFT,buffer2,CONV{1}_OUT_X,CONV{1}_OUT_Y,(q15_t*)col_buffer, NULL);\n".format(layer_no, layer_no + 1))
+        f.write("  arm_relu_q7(buffer2,CONV{0}_OUT_X*CONV{0}_OUT_Y*CONV{0}_OUT_CH);\n".format(layer_no + 1))
+
+        f.write("  //Pointwise conv\n")
+        f.write(
+          "  arm_convolve_1x1_HWC_q7_fast_nonsquare(buffer2, CONV{1}_OUT_X, CONV{1}_OUT_Y, CONV{0}_OUT_CH, conv{1}_pw_wt, CONV{1}_OUT_CH, 1, 1, 0, 0, 1, 1, conv{1}_pw_bias, CONV{1}_PW_BIAS_LSHIFT, CONV{1}_PW_OUT_RSHIFT, buffer1, CONV{1}_OUT_X, CONV{1}_OUT_Y, (q15_t*)col_buffer, NULL);\n".format(layer_no, layer_no + 1))
+        f.write(
+          "  arm_relu_q7(buffer1,CONV{0}_OUT_X*CONV{0}_OUT_Y*CONV{0}_OUT_CH);\n\n".format(layer_no + 1))
+
+    f.write("  //Average pool\n")
+    f.write(
+      "  arm_avepool_q7_HWC_nonsquare (buffer1,CONV{0}_OUT_X,CONV{0}_OUT_Y,CONV{0}_OUT_CH,CONV{0}_OUT_X,CONV{0}_OUT_Y,0,0,1,1,1,1,NULL,buffer2, AVG_POOL_OUT_LSHIFT);\n".format(num_layers))
+    f.write(
+      "  arm_fully_connected_q7(buffer2, final_fc_wt, CONV{0}_OUT_CH, OUT_DIM, FINAL_FC_BIAS_LSHIFT, FINAL_FC_OUT_RSHIFT, final_fc_bias, out_data, (q15_t*)col_buffer);\n".format(num_layers))
+    f.write("}\n")
---
+++ quant_dump.py
@@ -0,0 +1,316 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2017 Tanel Peet. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+#
+# Modifications Copyright 2017-2018 Arm Inc. All Rights Reserved.
+# Adapted from freeze.py to run quantized inference on train/val/test dataset on the
+# trained model in the form of checkpoint
+#
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os.path
+import sys
+import numpy as np
+
+import tensorflow as tf
+import input_data
+import quant_models as models
+import quant_act_max
+
+import helper
+
+def run_quant_inference(wanted_words, sample_rate, clip_duration_ms,
+                           window_size_ms, window_stride_ms, dct_coefficient_count,
+                           model_architecture, model_size_info):
+  """Creates an audio model with the nodes needed for inference.
+
+  Uses the supplied arguments to create a model, and inserts the input and
+  output nodes that are needed to use the graph for inference.
+
+  Args:
+    wanted_words: Comma-separated list of the words we're trying to recognize.
+    sample_rate: How many samples per second are in the input audio files.
+    clip_duration_ms: How many samples to analyze for the audio pattern.
+    window_size_ms: Time slice duration to estimate frequencies from.
+    window_stride_ms: How far apart time slices should be.
+    dct_coefficient_count: Number of frequency bands to analyze.
+    model_architecture: Name of the kind of model to generate.
+    model_size_info: Model dimensions : different lengths for different models
+  """
+
+  act_max = FLAGS.act_max
+  for maxium in act_max:
+    if maxium == 0:
+      print('Calling quant_act_max.py to get best act_max')
+      quant_act_max.FLAGS = FLAGS
+      act_max = quant_act_max.get_best_act_max(act_max)
+
+  tf.logging.set_verbosity(tf.logging.INFO)
+  sess = tf.InteractiveSession()
+  words_list = input_data.prepare_words_list(wanted_words.split(','))
+  model_settings = models.prepare_model_settings(
+      len(words_list), sample_rate, clip_duration_ms, window_size_ms,
+      window_stride_ms, dct_coefficient_count)
+
+  label_count = model_settings['label_count']
+  fingerprint_size = model_settings['fingerprint_size']
+
+  fingerprint_input = tf.placeholder(
+      tf.float32, [None, fingerprint_size], name='fingerprint_input')
+
+  logits = models.create_model(
+      fingerprint_input,
+      model_settings,
+      model_architecture,
+      model_size_info,
+      act_max,
+      is_training=False)
+
+  ground_truth_input = tf.placeholder(
+      tf.float32, [None, label_count], name='groundtruth_input')
+
+  predicted_indices = tf.argmax(logits, 1)
+  expected_indices = tf.argmax(ground_truth_input, 1)
+  correct_prediction = tf.equal(predicted_indices, expected_indices)
+  confusion_matrix = tf.confusion_matrix(
+      expected_indices, predicted_indices, num_classes=label_count)
+  evaluation_step = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  models.load_variables_from_checkpoint(sess, FLAGS.checkpoint)
+
+
+  # Quantize weights to 8-bits using (min,max) and write to file
+  f = open('weights.h','wb')
+  f.close()
+
+  if model_architecture == "ds_cnn":
+    num_layers = model_size_info[0]
+    helper.write_ds_cnn_cpp_file('ds_cnn.cpp', num_layers)
+
+    ds_cnn_h_fname = "ds_cnn.h"
+    weights_h_fname = "ds_cnn_weights.h"
+
+    f = open(ds_cnn_h_fname,'wb')
+    f.close()
+
+    with open(ds_cnn_h_fname, 'a') as f:
+      helper.write_ds_cnn_h_beginning(f, wanted_words, sample_rate,
+          clip_duration_ms, window_size_ms, window_stride_ms,
+          dct_coefficient_count, model_size_info, act_max)
+
+    # Quantize weights to 8-bits using (min,max) and write to file
+    f = open(weights_h_fname, 'wb')
+    f.close()
+
+    total_layers = len(act_max)
+    layer_no = 1
+    weights_dec_bits = 0
+
+  for v in tf.trainable_variables():
+    var_name = str(v.name)
+    var_values = sess.run(v)
+    min_value = var_values.min()
+    max_value = var_values.max()
+    int_bits = int(np.ceil(np.log2(max(abs(min_value),abs(max_value)))))
+    dec_bits = 7-int_bits
+    # convert to [-128,128) or int8
+    var_values = np.round(var_values*2**dec_bits)
+    var_name = var_name.replace('/','_')
+    var_name = var_name.replace(':','_')
+    with open('weights.h','a') as f:
+      f.write('#define '+var_name+' {')
+    if(len(var_values.shape)>2): #convolution layer weights
+      transposed_wts = np.transpose(var_values,(3,0,1,2))
+    else: #fully connected layer weights or biases of any layer
+      transposed_wts = np.transpose(var_values)
+    with open('weights.h','a') as f:
+      transposed_wts.tofile(f,sep=", ",format="%d")
+      f.write('}\n')
+    # convert back original range but quantized to 8-bits or 256 levels
+    var_values = var_values/(2**dec_bits)
+    # update the weights in tensorflow graph for quantizing the activations
+    var_values = sess.run(tf.assign(v,var_values))
+    print(var_name+' number of wts/bias: '+str(var_values.shape)+\
+            ' dec bits: '+str(dec_bits)+\
+            ' max: ('+str(var_values.max())+','+str(max_value)+')'+\
+            ' min: ('+str(var_values.min())+','+str(min_value)+')')
+
+    if model_architecture == "ds_cnn":
+      conv_layer_no = layer_no // 2 + 1
+
+      wt_or_bias = 'BIAS'
+      if 'weights' in var_name:
+        wt_or_bias = 'WT'
+
+      with open(weights_h_fname, 'a') as f:
+        if conv_layer_no == 1:
+          f.write('#define CONV1_{} {{'.format(wt_or_bias))
+        elif conv_layer_no <= num_layers:
+          if layer_no % 2 == 0:
+            f.write('#define CONV{}_DS_{} {{'.format(conv_layer_no, wt_or_bias))
+          else:
+            f.write('#define CONV{}_PW_{} {{'.format(conv_layer_no, wt_or_bias))
+        else:
+          f.write('#define FINAL_FC_{} {{'.format(wt_or_bias))
+
+        transposed_wts.tofile(f, sep=", ", format="%d")
+        f.write('}\n')
+
+      if 'weights' in var_name:
+        weights_dec_bits = dec_bits
+
+      if 'biases' in var_name:
+        # if averege pool layer, go to the next one
+        if layer_no == total_layers - 2:
+          layer_no += 1
+
+        input_dec_bits = 7 - np.log2(act_max[layer_no - 1])
+        output_dec_bits = 7 - np.log2(act_max[layer_no])
+        weights_x_input_dec_bits = input_dec_bits + weights_dec_bits
+        bias_lshift = int(weights_x_input_dec_bits - dec_bits)
+        output_rshift = int(weights_x_input_dec_bits - output_dec_bits)
+        print("Layer no: {} | Bias Lshift: {} | Output Rshift: {}\n".format(layer_no, bias_lshift, output_rshift))
+        with open('ds_cnn.h', 'a') as f:
+          if conv_layer_no == 1:
+            f.write("#define CONV1_BIAS_LSHIFT {}\n".format(bias_lshift))
+            f.write("#define CONV1_OUT_RSHIFT {}\n".format(output_rshift))
+          elif conv_layer_no <= num_layers:
+            if layer_no % 2 == 0:
+              f.write("#define CONV{}_DS_BIAS_LSHIFT {}\n".format(conv_layer_no, bias_lshift))
+              f.write("#define CONV{}_DS_OUT_RSHIFT {}\n".format(conv_layer_no, output_rshift))
+            else:
+              f.write("#define CONV{}_PW_BIAS_LSHIFT {}\n".format(conv_layer_no, bias_lshift))
+              f.write("#define CONV{}_PW_OUT_RSHIFT {}\n".format(conv_layer_no, output_rshift))
+          else:
+            f.write("#define FINAL_FC_BIAS_LSHIFT {}\n".format(bias_lshift))
+            f.write("#define FINAL_FC_OUT_RSHIFT {}\n".format(output_rshift))
+
+        layer_no += 1
+
+  if model_architecture == "ds_cnn":
+    input_dec_bits = 7 - np.log2(act_max[len(act_max) - 3])
+    output_dec_bits = 7 - np.log2(act_max[len(act_max) - 2])
+    with open(ds_cnn_h_fname, 'a') as f:
+      f.write("#define AVG_POOL_OUT_LSHIFT {}\n\n".format(int(output_dec_bits - input_dec_bits)))
+      helper.write_ds_cnn_h_end(f, num_layers)
+
+def main(_):
+
+  # Create the model, load weights from checkpoint and dump
+  run_quant_inference(FLAGS.wanted_words, FLAGS.sample_rate,
+                      FLAGS.clip_duration_ms, FLAGS.window_size_ms,
+                      FLAGS.window_stride_ms, FLAGS.dct_coefficient_count,
+                      FLAGS.model_architecture, FLAGS.model_size_info)
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--data_url',
+      type=str,
+      # pylint: disable=line-too-long
+      default='http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz',
+      # pylint: enable=line-too-long
+      help='Location of speech training data archive on the web.')
+  parser.add_argument(
+      '--data_dir',
+      type=str,
+      default='/tmp/speech_dataset/',
+      help="""\
+      Where to download the speech training data to.
+      """)
+  parser.add_argument(
+      '--silence_percentage',
+      type=float,
+      default=10.0,
+      help="""\
+      How much of the training data should be silence.
+      """)
+  parser.add_argument(
+      '--unknown_percentage',
+      type=float,
+      default=10.0,
+      help="""\
+      How much of the training data should be unknown words.
+      """)
+  parser.add_argument(
+      '--validation_percentage',
+      type=int,
+      default=10,
+      help='What percentage of wavs to use as a validation set.')
+  parser.add_argument(
+      '--sample_rate',
+      type=int,
+      default=16000,
+      help='Expected sample rate of the wavs',)
+  parser.add_argument(
+      '--clip_duration_ms',
+      type=int,
+      default=1000,
+      help='Expected duration in milliseconds of the wavs',)
+  parser.add_argument(
+      '--window_size_ms',
+      type=float,
+      default=30.0,
+      help='How long each spectrogram timeslice is',)
+  parser.add_argument(
+      '--window_stride_ms',
+      type=float,
+      default=10.0,
+      help='How long each spectrogram timeslice is',)
+  parser.add_argument(
+      '--dct_coefficient_count',
+      type=int,
+      default=40,
+      help='How many bins to use for the MFCC fingerprint',)
+  parser.add_argument(
+      '--batch_size',
+      type=int,
+      default=100,
+      help='How many items to train with at once',)
+  parser.add_argument(
+      '--wanted_words',
+      type=str,
+      default='yes,no,up,down,left,right,on,off,stop,go',
+      help='Words to use (others will be added to an unknown label)',)
+  parser.add_argument(
+      '--checkpoint',
+      type=str,
+      default='',
+      help='Checkpoint to load the weights from.')
+  parser.add_argument(
+      '--model_architecture',
+      type=str,
+      default='dnn',
+      help='What model architecture to use')
+  parser.add_argument(
+      '--model_size_info',
+      type=int,
+      nargs="+",
+      default=[128,128,128],
+      help='Model dimensions - different for various models')
+  parser.add_argument(
+      '--act_max',
+      type=float,
+      nargs="+",
+      default=[128,128,128],
+      help='activations max')
+
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
